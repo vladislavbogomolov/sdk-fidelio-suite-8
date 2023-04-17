560592851498054b860b3be3cad5d7ac4ea9827f
Vladislav Bogomolov <bogomolov.vlad@gmail.com>
2023-04-17 12:13:15 +0200

    save

---
+++ src/config/connections.ts
@@ -0,0 +1,24 @@
+type IConnection = {
+    IS_MASTER: boolean;
+    USER_AGENT: string;
+    FIDELIO_VERSION: string;
+    FIDELIO_USERNAME: string;
+    DEBUG_ENABLED: boolean;
+    FIDELIO_VENDOR: string;
+    FIDELIO_PASSWORD: string;
+    CODE: string;
+    URL: string
+}
+export const Connections: IConnection[] = [
+    {
+        URL: "",
+        CODE: "",
+        FIDELIO_VERSION: "1.1.0",
+        FIDELIO_VENDOR: "sdk-fidelio-suite",
+        FIDELIO_USERNAME: "",
+        FIDELIO_PASSWORD: "",
+        USER_AGENT: "FidelioNodeService",
+        DEBUG_ENABLED: false,
+        IS_MASTER: true
+    }
+]
