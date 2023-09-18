58c2f19f7e9e6f486e987b5f4eb5119ee2109441
Vladislav Bogomolov <bogomolov.vlad@gmail.com>
2023-09-18 12:51:50 +0200

    save

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,2 @@
 import {Fidelio} from "./Fidelio";
-import {IFieldsRequestAvailabilityForWeb} from "./interfaces/availability";
-import {Connections} from "./config/connections";
-
-const reqFields: IFieldsRequestAvailabilityForWeb = {
-    GuestArrival: new Date('2024-06-01'),
-    GuestDeparture: new Date('2024-06-03'),
-    NoOfAdults: 2,
-    MultipleRates: 1,
-    AvailabilityLimit: 'BookingOnline',
-    WebOnly: 1
-}
-
-const connection = new Fidelio(Connections[0]);
-connection.AvailabilityForWeb.where(reqFields).get().then((response) => {
-    console.log(JSON.stringify(response))
-})
-
 export {Fidelio}
