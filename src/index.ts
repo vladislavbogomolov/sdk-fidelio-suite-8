cdae09fae79ce38c50669017218a9d7df1b76c3b
Vladislav Bogomolov <vladislavbogomolov@MacBook-Pro-di-Vladislav.local>
2024-01-12 16:39:47 +0800

    save

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,3 @@
 import {Fidelio} from "./Fidelio";
 
-const FidelioClient = new Fidelio({
-
-});
-
-
-FidelioClient.Reservation.where('GuestNum', 1).set({
-    BookerID: 1,
-    ProfileID: 1,
-    ReservationStatus: 1
-}).save().then()
-
 export {Fidelio}
